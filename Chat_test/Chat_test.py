592ff38c74b2fbaadd2b7cac695c9e78ec39d642
Balnian <super_f_qc@hotmail.com>
2015-01-15 17:07:21 -0500

    Interface

--- Chat_test/Chat_test.py
+++ Chat_test/Chat_test.py
@@ -1,10 +1,14 @@
 import sfml.network as sfn
 import _thread
 import string
+import tkinter as tk
 
-def HandShake():
+def HandShake(root):
     tcpList = sfn.TcpListener()
-    tcpList.listen(55002)
+    tcpList.listen(55001)
+    w = tk.Label(root, text="Port Locale : {0}".format(tcpList.local_port.__str__()))
+    w.pack()
+    print("Port Locale : {0}".format(tcpList.local_port.__str__()))
     tcpSock = sfn.TcpSocket()
     while(1):
         try:
@@ -15,27 +19,40 @@ def HandShake():
             print("An error occured! Error: {0}".format(error))
             exit(1)
         
-
+    message=[]
     while(1):
-        message = tcpSock.receive(1024)
-        print("1")
-        print(tcpSock.remote_address.__str__()+" : "+message.decode('utf-8'))
+        message.append(tcpSock.receive(1024))
+       # print("1")
+        #print(tcpSock.remote_address.__str__()+" : "+message.decode('utf-8'))
+        MyText = tk.scrolledtext(root,width=300)
+        MyText.pack()
+        for i in range(message.__len__):
+            MyText.insert(tk.END,message(i))
+
 
 #def Listen(Sock):
 
+root = tk.Tk()
+ViewFrame = tk.Frame(root)
+Screen = tk.scrolledtext(root,width,)
+w = tk.Label(root, text="allo")
+w.pack()
+
 print("Vptre Adresse IP: {0}".format(sfn.IpAddress.get_local_address()))
-_thread.start_new_thread(HandShake,())
+_thread.start_new_thread(HandShake,(root,))
+root.mainloop()
 print("Entrer la chaine de connection:")
 inp = input()
 
 ip = sfn.IpAddress.from_string(inp)
-tcpSock = sfn.TcpSocket()
-tcpSock.connect(ip,55001)
-tcpSock.send("Hi, I am a client".encode('utf-8'))
+tcpSock2 = sfn.TcpSocket()
+tcpSock2.connect(ip,55002)
+
+tcpSock2.send("Hi, I am a client".encode('utf-8'))
 
 while(1):
     inp = input()
-    tcpSock.send(inp.encode('utf-8'))
+    tcpSock2.send(inp.encode('utf-8'))
 
 #tcpSock = HandShake()
 
