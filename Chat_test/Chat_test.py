c200d3e5e12c87faa763a9b868ba865f6c381327
Balnian <super_f_qc@hotmail.com>
2015-01-15 17:40:05 -0500

    Scrolled text enabled

--- Chat_test/Chat_test.py
+++ Chat_test/Chat_test.py
@@ -1,13 +1,17 @@
 import sfml.network as sfn
 import _thread
 import string
-import tkinter as tk
+#import tkinter as tk
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 
 def HandShake(root):
     tcpList = sfn.TcpListener()
     tcpList.listen(55001)
-    w = tk.Label(root, text="Port Locale : {0}".format(tcpList.local_port.__str__()))
+    w = Label(root, text="Port Locale : {0}".format(tcpList.local_port.__str__()))
     w.pack()
+    MyText = ScrolledText(root)
+    MyText.pack()
     print("Port Locale : {0}".format(tcpList.local_port.__str__()))
     tcpSock = sfn.TcpSocket()
     while(1):
@@ -24,35 +28,37 @@ def HandShake(root):
         message.append(tcpSock.receive(1024))
        # print("1")
         #print(tcpSock.remote_address.__str__()+" : "+message.decode('utf-8'))
-        MyText = tk.scrolledtext(root,width=300)
+        MyText = ScrolledText(root,width=300)
         MyText.pack()
         for i in range(message.__len__):
-            MyText.insert(tk.END,message(i))
+            MyText.insert(END,message(i))
 
 
 #def Listen(Sock):
 
-root = tk.Tk()
-ViewFrame = tk.Frame(root)
-Screen = tk.scrolledtext(root,width,)
-w = tk.Label(root, text="allo")
+root = Tk()
+#ViewFrame = Frame(root)
+#Screen = ScrolledText(root)
+#Screen.pack()
+w = Label(root, text="allo")
 w.pack()
 
 print("Vptre Adresse IP: {0}".format(sfn.IpAddress.get_local_address()))
 _thread.start_new_thread(HandShake,(root,))
-root.mainloop()
+
 print("Entrer la chaine de connection:")
 inp = input()
 
 ip = sfn.IpAddress.from_string(inp)
 tcpSock2 = sfn.TcpSocket()
-tcpSock2.connect(ip,55002)
+tcpSock2.connect(ip,55001)
 
 tcpSock2.send("Hi, I am a client".encode('utf-8'))
 
-while(1):
-    inp = input()
-    tcpSock2.send(inp.encode('utf-8'))
+#while(1):
+inp = input()
+tcpSock2.send(inp.encode('utf-8'))
+root.mainloop()
 
 #tcpSock = HandShake()
 
